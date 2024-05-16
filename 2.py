7a5f29030f2793f49acf46f65e4781e1b1575eef
AShMOo <ashrafaltarabin2000@gmail.com>
2024-05-16 01:49:13 -0400

    first commit

---
+++ 2.py
@@ -0,0 +1,29 @@
+# question 2 task 1
+num1 = float(input("enter the first number: "))
+num2 = float(input("enter the second number: "))
+num3 = float(input("enter the third number: "))
+
+if num1 >= num2 and num1 >= num3:
+    largest = num1
+elif num2 >= num1 and num2 >= num3:
+    largest = num2
+else:
+    largest = num3
+print ("the largest number is:" , largest)
+
+
+
+# Task 2
+num1 = float(input("enter the first number: "))
+num2 = float(input("enter the second number: "))
+num3 = float(input("enter the third number: "))
+
+if num1 <= num2 and num1 <= num3:
+    smallest = num1
+elif num2 <= num1 and num2 <= num3:
+    smallest = num2
+else:
+    smallest = num3
+print ("the smallest number is:" , smallest)
+
+
---
+++ 3.py
@@ -0,0 +1,11 @@
+#Assignment 3 task 1
+year = int(input("enter a year: "))
+if (year % 4 == 0 and year % 100 !=0) or year%400==0  : 
+     print (f"{year} is a leap year.")
+else:
+     print (f"{year} is not a leap year.")
+
+
+
+            
+
